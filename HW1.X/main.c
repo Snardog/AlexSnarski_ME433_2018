b943e7a417f9200df217ab526e91a5398f636ebf
Snardog <alexandersnarski2018@u.northwestern.edu>
2017-03-30 12:07:34 -0500

    Changed values for config
    Orginally tried to se the bits, changed to things like OFF and ON etc

--- HW1.X/main.c
+++ HW1.X/main.c
@@ -2,39 +2,39 @@
 #include<sys/attribs.h>  // __ISR macro
 
 // DEVCFG0
-#pragma config DEBUG = 0 // no debugging
-#pragma config JTAGEN = 0 // no jtag
-#pragma config ICESEL = 0b11 // use PGED1 and PGEC1
-#pragma config PWP = 0x1FF // no write protect
-#pragma config BWP = 0 // no boot write protect
-#pragma config CP = 0 // no code protect
+#pragma config DEBUG = OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // no boot write protect
+#pragma config CP = OFF // no code protect
 
 // DEVCFG1
-#pragma config FNOSC = 0b011 // use primary oscillator with pll
-#pragma config FSOSCEN = 0 // turn off secondary oscillator
-#pragma config IESO = 0 // no switching clocks
-#pragma config POSCMOD = 0b10 // high speed crystal mode
-#pragma config OSCIOFNC = 0 // free up secondary osc pins
-#pragma config FPBDIV = 0 // divide CPU freq by 1 for peripheral bus clock
-#pragma config FCKSM = 0b11 // do not enable clock switch
-#pragma config WDTPS = 0 // slowest wdt
-#pragma config WINDIS = 0 // no wdt window
-#pragma config FWDTEN = 0 // wdt off by default
-#pragma config FWDTWINSZ = 0b11 // wdt window at 25%
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // free up secondary osc pins
+#pragma config FPBDIV = DIV_1 // divide CPU freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1048576 // slowest wdt
+#pragma config WINDIS = OFF // no wdt window
+#pragma config FWDTEN = OFF // wdt off by default
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
 
 // DEVCFG2 - get the CPU clock to 48MHz
-#pragma config FPLLIDIV = 0b001 // divide input clock to be in range 4-5MHz
-#pragma config FPLLMUL = 0b111 // multiply clock after FPLLIDIV
-#pragma config FPLLODIV = 0b001 // divide clock after FPLLMUL to get 48MHz
-#pragma config UPLLIDIV = 0b001 // divider for the 8MHz input clock, then multiply by 12 to get 48MHz for USB
-#pragma config UPLLEN = 0 // USB clock on
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+#pragma config UPLLIDIV = DIV_2 // divider for the 8MHz input clock, then multiply by 12 to get 48MHz for USB
+#pragma config UPLLEN = OFF // USB clock on
 
 // DEVCFG3
 #pragma config USERID = 0 // some 16bit userid, doesn't matter what
-#pragma config PMDL1WAY = 0 // allow multiple reconfigurations
-#pragma config IOL1WAY = 0 // allow multiple reconfigurations
-#pragma config FUSBIDIO = 1 // USB pins controlled by USB module
-#pragma config FVBUSONIO = 1 // USB BUSON controlled by USB module
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+#pragma config FUSBIDIO = ON // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
 
 
 int main() {
@@ -54,11 +54,22 @@ int main() {
     DDPCONbits.JTAGEN = 0;
 
     // do your TRIS and LAT commands here
-
+    TRISAbits.TRISA4 = 0; // A4 is a digital output
+    TRISBbits.TRISB4 = 1; // B4 is output compare
     __builtin_enable_interrupts();
 
     while(1) {
 	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 		  // remember the core timer runs at half the CPU speed
+        _CP0_SET_COUNT(0);
+        LATAbits.LATA4 = 0;
+        while (_CP0_GET_COUNT() < 12000) { // .5ms loop before LED turns off
+            ;
+        }
+        LATAbits.LATA4 = 1;
+        _CP0_SET_COUNT(0);
+        while (_CP0_GET_COUNT() < 12000) { // .5ms loop before LED turns back on
+            ;
+        }
     }
 }
