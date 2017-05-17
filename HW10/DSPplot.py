aa8994008e448c23bf9bdbd3abb10b49566f9bc7
Snardog <alexandersnarski2018@u.northwestern.edu>
2017-05-17 10:39:40 -0500

    Added Homework 10, started homework 14
    Homework 12 and 13 are also finished, I just need to upload them from my
    desktop

---
+++ HW10/DSPplot.py
@@ -0,0 +1,30 @@
+import serial
+import matplotlib.pyplot as plt\
+
+ser = serial.Serial('COM5',9600)
+print(ser.name)
+
+ser.write('r')
+
+raw = []
+MAF = []
+IIR = []
+FIR = []
+
+for i in range(99):
+	serial_line = ser.readline()
+	data_read = serial_line.split(",")
+	print(i)
+	print(float(data_read[1]), flat(data_read[2]), float(data_read[3]), float(data_read[4]))
+	raw.append(float(data_read[1]))
+	MAF.append(float(data_read[2]))
+	IIR.append(float(data_read[3]))
+	FIR.append(float(data_read[4]))
+
+plt.plot(raw,label='raw')
+plt.plot(MAF,label='MAF')
+plt.plot(IIR,label='IIR')
+plt.plot(FIR,label='FIR')
+plt.legend()
+plt.show()
+	
